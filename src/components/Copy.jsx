2fa5d33bf38715e527ae7373e0d61e2916c7bee3
Yaman Ayush <yamanayush27204@gmail.com>
2025-03-28 12:02:00 +0530

    Update Copy.jsx

--- src/components/Copy.jsx
+++ src/components/Copy.jsx
@@ -2,7 +2,7 @@ const Copy = () => {
   return (
 
 
-      <p className="mb-1 text-neutral-400"><center> Copyright © 2024 - Yaman Ayush All Rights Reserved. </center></p>
+      <p className="mb-1 text-neutral-400"><center> Copyright © 2025 - Yaman Ayush All Rights Reserved. </center></p>
 
   );
 };
