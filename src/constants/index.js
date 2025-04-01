279d0b05b1f54b9800c8a21df48bec9dbb6560bf
yamanayush <yamanayush27204@gmail.com>
2025-04-01 12:42:04 +0530

    Resolved merge conflict in constants.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,19 +2,13 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
-<<<<<<< HEAD
-=======
 import project5 from "../assets/projects/project-5.jpg";
->>>>>>> e3f68fe (update on portfolio from 2024-2025)
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile software and full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile software and full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
-<<<<<<< HEAD
-=======
-
   {
     year: "February 2025 - Present",
     role: "Software Developer Intern",
@@ -31,20 +25,11 @@ export const EXPERIENCES = [
     technologies: ["MongoDB", "Express.js", "React.js", "Node.js"],
   },
 
->>>>>>> e3f68fe (update on portfolio from 2024-2025)
   {
     year: "June 2023 - July 2023",
-    role: "Traniee",
+    role: "Trainee",
     company: "Board Infinity Inc.",
-<<<<<<< HEAD
-    description: `Acquired expertise in JavaScript, spanning from fundamentals to advanced concepts such as async, promises, and callbacks, 
-showcasing a comprehensive skill set. Developed 3 mini projects—Id Card Generator, Random Quote Generator, and Clock— illustrating creative problem solving and 
-the ability to translate theoretical knowledge into practical, real-world applications. `,
-    technologies: ["JavaScript"],
-  },
-
-=======
-    description: `Acquired expertise in JavaScript, spanning from fundamentals to advanced concepts such as async, promises, and callbacks, showcasing a comprehensive skill set. Developed 3 mini projects—Id Card Generator, Random Quote Generator, and Clock— illustrating creative problem solving and the ability to translate theoretical knowledge into practical, real-world applications. `,
+    description: `Acquired expertise in JavaScript, spanning from fundamentals to advanced concepts such as async, promises, and callbacks, showcasing a comprehensive skill set. Developed 3 mini projects—ID Card Generator, Random Quote Generator, and Clock—illustrating creative problem-solving and the ability to translate theoretical knowledge into practical, real-world applications.`,
     technologies: ["JavaScript"],
   },
 
@@ -52,78 +37,50 @@ the ability to translate theoretical knowledge into practical, real-world applic
     year: "June 2022 - July 2022",
     role: "Web Developer Intern",
     company: "Drishya Welfare Society",
-    description:`Conceptualized and engineered a highly intuitive and responsive website for Drishya Welfare Society, substantially bolstering their digital presence by directing a team of volunteers to produce compelling content that engaged over 10,000 individuals. Added a seamless Donation Portal, catalyzing a 25% surge in online contribution.`,
+    description: `Conceptualized and engineered a highly intuitive and responsive website for Drishya Welfare Society, substantially bolstering their digital presence by directing a team of volunteers to produce compelling content that engaged over 10,000 individuals. Added a seamless Donation Portal, catalyzing a 25% surge in online contributions.`,
     technologies: ["JavaScript", "HTML", "CSS"],
   }
-
->>>>>>> e3f68fe (update on portfolio from 2024-2025)
 ];
 
 export const PROJECTS = [
   {
     title: "ChatGPT AI Voice Chatbot",
     image: project1,
-    description:
-<<<<<<< HEAD
-      " Deployed RACHEL CHAT, a versatile AI chatbot powered by ChatGPT, for diverse applications including sales, language learning, and entertainment.",
-    technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python"],
-  },
-  {
-    title: "Project Management Dashboard",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-=======
-      "Deployed RACHEL CHAT, a versatile AI chatbot powered by ChatGPT, for diverse applications including sales, language learning, and entertainment.",
+    description: "Deployed RACHEL CHAT, a versatile AI chatbot powered by ChatGPT, for diverse applications including sales, language learning, and entertainment.",
     technologies: ["CSS", "JavaScript", "React", "Node.js", "Python"],
   },
   {
-    title: "Mini Loan Application ",
+    title: "Mini Loan Application",
     image: project2,
     link: "https://loan-app-gilt.vercel.app/",
-    description:
-      "Developing a Mini-Loan App with a separate client and server structure, allowing users to apply for loans and manage repayments while admins handle approvals.",
-    technologies: ["Mern-Stack", "Tailwind CSS", "TypeScript"],
+    description: "Developing a Mini-Loan App with a separate client and server structure, allowing users to apply for loans and manage repayments while admins handle approvals.",
+    technologies: ["MERN Stack", "Tailwind CSS", "TypeScript"],
   },
   {
     title: "Digital Library Management System",
     image: project3,
     link: "https://digital-library-system.vercel.app/",
-    description:
-      "An application for managing a digital library, with features such as book cataloging, user management, and borrowing tracking",
->>>>>>> e3f68fe (update on portfolio from 2024-2025)
-    technologies: ["Mern-Stack", "Tailwind CSS", "TypeScript"],
+    description: "An application for managing a digital library, with features such as book cataloging, user management, and borrowing tracking.",
+    technologies: ["MERN Stack", "Tailwind CSS", "TypeScript"],
   },
   {
     title: "Drishya Non-Governmental Organization Website",
-<<<<<<< HEAD
-    image: project3,
-    description:
-      " Designed and developed a user-friendly and responsive website for Drishya Welfare Society, enhancing their online presence.",
-=======
     image: project4,
     link: "https://yamanayush.github.io/Drishya-Welfare-Society/",
-    description:
-      "Designed and developed a user-friendly and responsive website for Drishya Welfare Society, enhancing their online presence.",
->>>>>>> e3f68fe (update on portfolio from 2024-2025)
+    description: "Designed and developed a user-friendly and responsive website for Drishya Welfare Society, enhancing their online presence.",
     technologies: ["HTML", "CSS"],
   },
   {
     title: "Personal Portfolio",
-<<<<<<< HEAD
-    image: project4,
-=======
     image: project5,
     link: "https://yaman-portfolio.vercel.app/",
->>>>>>> e3f68fe (update on portfolio from 2024-2025)
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information",
+    description: "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["React", "HTML", "Tailwind CSS", "JavaScript", "Node.js"],
   },
 ];
 
 export const CONTACT = {
-  address: "Ratlam, Madhya Pradesh, India ",
-  phoneNo: "+91 77269 37417 ",
+  address: "Ratlam, Madhya Pradesh, India",
+  phoneNo: "+91 77269 37417",
   email: "yamanayush27204@gmail.com",
 };
